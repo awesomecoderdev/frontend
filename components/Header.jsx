74c82b0ca9b3d40dcf6fd413c8345411c2c22568
Mohammad Ibrahim <awesomecoder.dev@gmail.com>
2022-11-02 01:25:44 +0600

    Startup Frontend

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,8 +24,28 @@ const Header = ({authCheck}) => {
     return (
         <Fragment>
             <header className="relative w-full">
-                {/* <h1>Hellow {user.email}</h1> */}
-                {authCheck ? <>you are login</> :<>You are not login</>}
+                <nav class="block w-full rounded-xl shadow-md backdrop-saturate-200 backdrop-blur-2xl bg-opacity-80 border border-white/80 bg-white text-gray-500 mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+                    <div class="container mx-auto flex items-center justify-between text-blue-gray-900">
+                        <a href="#" class="block antialiased font-sans text-sm leading-normal text-inherit mr-4 cursor-pointer py-1.5 font-normal">
+                            <span>Material Tailwind</span>
+                        </a>
+                        <ul class="hidden items-center gap-6 lg:flex">
+                            <li class="block antialiased font-sans text-sm leading-normal text-inherit p-1 font-normal"><a class="flex items-center" href="#">Pages</a></li>
+                            <li class="block antialiased font-sans text-sm leading-normal text-inherit p-1 font-normal"><a class="flex items-center" href="#">Account</a></li>
+                            <li class="block antialiased font-sans text-sm leading-normal text-inherit p-1 font-normal"><a class="flex items-center" href="#">Blocks</a></li>
+                            <li class="block antialiased font-sans text-sm leading-normal text-inherit p-1 font-normal"><a class="flex items-center" href="#">Docs</a></li>
+                            </ul>
+                        {authCheck ?
+                            <button class="middle none font-sans font-bold center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 rounded-lg bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-md shadow-blue-500/20 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] hidden lg:inline-block" type="button">
+                                <span>Dashboard</span>
+                            </button>
+                        :
+                            <button class="middle none font-sans font-bold center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 rounded-lg bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-md shadow-blue-500/20 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] hidden lg:inline-block" type="button">
+                                <span>Login</span>
+                            </button>
+                        }
+                    </div>
+                </nav>
             </header>
         </Fragment>
     );
