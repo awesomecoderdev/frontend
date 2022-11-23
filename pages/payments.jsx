b42209ac75c3fb4eb37f7a01875590f224c36259
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2022-11-23 19:54:57 +0600

    Updated payments page

--- pages/payments.jsx
+++ pages/payments.jsx
@@ -2,12 +2,56 @@ import Head from "next/head";
 import React, { Fragment, useState } from "react";
 import {
 	CheckCircleIcon,
+	CreditCardIcon,
 	DocumentTextIcon,
+	EnvelopeOpenIcon,
 	ExclamationCircleIcon,
 } from "@heroicons/react/24/outline";
 import Topbar from "../components/Topbar";
 import Localstorage from "../lib/localstorage";
 
+const payments = [
+	{
+		id: "1",
+		transaction_id: "AD0D689Osdfsdfsdfsdf",
+		name: "John Doe",
+		email: "johndoe@example.com",
+		phone: "+1 (921) 5132012",
+		created_at: "16-5-2015 9:50:89",
+		amount: 24,
+		status: "success",
+	},
+	{
+		id: "2",
+		transaction_id: "AD0D6895",
+		name: "John Doe 2",
+		email: "johndoe@example.com",
+		phone: "+1 (921) 5132012",
+		created_at: "16-5-2015 9:50:89",
+		amount: 24,
+		status: "success",
+	},
+	{
+		id: "3",
+		transaction_id: "AD0D6899",
+		name: "John Doe 3",
+		created_at: "16-5-2015 9:50:89",
+		amount: 24,
+		email: "johndoe@example.com",
+		phone: "+1 (921) 5132012",
+		status: "canceled",
+	},
+	{
+		id: "4",
+		transaction_id: "AD0D6891",
+		name: "John Doe 4",
+		email: "johndoe@example.com",
+		phone: "+1 (921) 5132012",
+		amount: 24,
+		created_at: "16-5-2015 9:50:89",
+		status: "success",
+	},
+];
 const Payments = () => {
 	const { name, email, user, verified } = Localstorage();
 	const [tab, setTab] = useState("all");
@@ -62,6 +106,84 @@ const Payments = () => {
 							</button>
 						</li>
 					</ul>
+				</div>
+
+				<div className="relative pt-4">
+					{payments.map((payment) => {
+						return (
+							<Fragment key={payment.id}>
+								<div
+									className={`${
+										payment.read_at === null
+											? "bg-primary-50/50"
+											: "bg-white"
+									} relative flex items-center w-full border mb-3 border-slate-100 rounded-md`}
+								>
+									<button className="relative flex justify-center items-center m-3 w-15 h-15 rounded-full text-primary-500 ">
+										<h2 className="text-slate-600 font-semibold text-sm flex justify-center items-center ">
+											<CreditCardIcon className="h-4 w-4" />
+										</h2>
+										<span
+											className={`${
+												payment.status == "success"
+													? "bg-green-400"
+													: payment.status ==
+													  "canceled"
+													? "bg-red-400"
+													: "bg-blue-400"
+											} absolute -left-1 top-0 h-2.5 w-2.5 border-white border-2  rounded-full`}
+										></span>
+									</button>
+									<div className="relative md:pl-0 pl-2 h-full overflow-hidden pr-10">
+										<p className="text-xs truncate w-20 font-semibold text-slate-500/80">
+											#{payment?.transaction_id}
+										</p>
+									</div>
+
+									<div className="relative flex items-center justify-evenly w-full md:pl-0 pl-2 h-full overflow-hidden pr-10">
+										<p
+											className={` ${
+												payment.status == "success"
+													? "text-green-400"
+													: payment.status ==
+													  "canceled"
+													? "text-red-400"
+													: "text-primary-400"
+											} text-xs truncate w-20 text-center font-semibold `}
+										>
+											${payment?.amount} USD
+										</p>
+
+										<h1 className="text-xs font-semibold text-slate-500/80">
+											{payment.created_at
+												? payment.created_at
+												: ""}
+										</h1>
+									</div>
+									<div className="relative flex items-center justify-evenly w-full md:pl-0 pl-2 h-full overflow-hidden pr-10">
+										<p
+											className={` ${
+												payment.status == "success"
+													? "text-green-400"
+													: payment.status ==
+													  "canceled"
+													? "text-red-400"
+													: "text-primary-400"
+											} text-xs truncate w-20 text-center font-semibold `}
+										>
+											${payment?.amount} USD
+										</p>
+
+										<h1 className="text-xs font-semibold text-slate-500/80">
+											{payment.created_at
+												? payment.created_at
+												: ""}
+										</h1>
+									</div>
+								</div>
+							</Fragment>
+						);
+					})}
 				</div>
 			</div>
 		</Fragment>
