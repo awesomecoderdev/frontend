bf60fadfbb191fec9d32e5dab9a5766b4ee9dd50
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2022-12-07 17:06:29 +0600

    Updated Domain

--- lib/request.jsx
+++ lib/request.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-	baseURL: "https://api.wpplagiarism.dev/v1/",
+	baseURL: "https://api.wpplagiarism.one/v1/",
 	withCredentials: true,
 	headers: {
 		"X-Requested-With": "XMLHttpRequest",
--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: false,
-  // swcMinify: true,
-  images: {
-    domains: [
-      "awesomecoder.dev",
-      "wpplagiarism.com",
-      "localhost",
-      "lh3.googleusercontent.com"
-    ],
-    formats: ["image/avif", "image/webp"],
-  },
+	reactStrictMode: false,
+	// swcMinify: true,
+	images: {
+		domains: [
+			"awesomecoder.dev",
+			"wpplagiarism.one",
+			"localhost",
+			"lh3.googleusercontent.com",
+		],
+		formats: ["image/avif", "image/webp"],
+	},
 };
-
-
